cc62f0959d05699252b7c6b63411602ef4793193
Merdescu Razvan <merdescu.razvan@gmail.com>
2015-11-16 19:12:48 +0200

    Update IngAPI.java

--- IngAPI.java
+++ IngAPI.java
@@ -4,24 +4,22 @@ import java.net.URL;
 //Must import JSON Object
 
 public class IngAPI {
-	
 	public String getBasic() throws Exception{
 		return PrintcURL("");
 	}
 	public String PrintcURL(String option) throws Exception {
-        URL oracle = new URL("http://213.157.170.77/"+option);
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
+ 		URL oracle = new URL("http://213.157.170.77/"+option);
+        	BufferedReader in = new BufferedReader(
+        	new InputStreamReader(oracle.openStream()));
 
-        String inputLine;
-        String ObjectString="";
-        while ((inputLine = in.readLine()) != null){
-        	ObjectString+=inputLine;
-        }
+        	String inputLine;
+        	String ObjectString="";
+        	while ((inputLine = in.readLine()) != null){
+        		ObjectString+=inputLine;
+        	}
         in.close();
         return ObjectString;
        
-    }
-
+    	}
 }
 
